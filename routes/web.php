8db305f88f56245ed18c77f09d71d25f0c6379cd
Klvst3r <klvst3r@gmail.com>
2023-08-02 15:19:06 -0600

    1.1. Rutas con cnombre

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     //return view('welcome');
     return "Hola desde la pagina de inicio";
+});*/
+
+Route::get('/', function () {
+    echo "<a href=" . route('contactos') . ">Contacto</a><br/>";
+    echo "<a href=" . route('contactos') . ">Contacto</a><br/>";
+    echo "<a href=" . route('contactos') . ">Contacto</a><br/>";
+    echo "<a href=" . route('contactos') . ">Contacto</a><br/>";
+   
 });
 
-Route::get('contacto', function(){
+Route::get('contactanos', ['as' => 'contactos', function(){
     return "Hola desde la pagina de contacto";     
-    });
+    }]);
 
 /*Route::get('saludos/{nombre}', function($nombre){
         return "Saludos $nombre";
     });*/
 
-Route::get('saludos/{nombre?}', function($nombre = "Invitado"){
+/*Route::get('saludos/{nombre?}', function($nombre = "Invitado"){
         return "Saludos $nombre";
-    });
+    });*/
+
+
+Route::get('saludos/{nombre?}', function($nombre = "Invitado"){
+
+        return "Saludos $nombre";
+
+    })->where('nombre', "[A-Za-z]+");
