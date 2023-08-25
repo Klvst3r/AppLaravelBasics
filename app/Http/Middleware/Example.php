9b4747e32243184ce7823d546703e8bce461e597
Klvst3r <klvst3r@gmail.com>
2023-08-25 09:22:47 -0600

    5.4. Primera forma de utilizar el middleware Example

---
+++ app/Http/Middleware/Example.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Example
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        
+        if(false){
+
+        return $next($request);
+
+        }
+
+        return response('No puedes continuar', 404);
+    }
+}
