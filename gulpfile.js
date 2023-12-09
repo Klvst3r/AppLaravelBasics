04fca41b994b809f5b8f6db0cd9757d4c0970393
Klvst3r <klvst3r@gmail.com>
2023-12-08 23:12:51 -0600

    8.3. Instalando Elixir

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+//var elixir = require('laravel-elixir'); 
+
+/*
+/--------------------------------
+/ Elixir Asset Management
+/--------------------------------
+/
+/ Elixir provides a clean, fluent API for definiting some basic Gulp task
+/ for your Laravel application. Be default, we are compiling tha Sas
+/ file for our application, as well as publishing vendor resources.
+/
+ */
+/*elixir(function(mix){
+	mix.sass('app.scss');
+});*/
